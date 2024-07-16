0826615aa9f81c69eabb9726f4f90f476c1cb225
Amilzithios <amilzithios@gmail.com>
2024-07-16 23:21:42 +0530

    Initial Commit

---
+++ Basic UIKIT/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Basic UIKIT
+//
+//  Created by Amilzith on 16/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
